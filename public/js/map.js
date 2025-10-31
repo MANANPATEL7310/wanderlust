58ee465964c149d8d8f95b087bbb2b07e87e4f9f
Manan patel <Manan patel>
2025-10-31 19:34:23 +0530

    prevent auto-scroll of show.ejs

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,7 @@
 let mapboxToken = window.mapboxToken;
 mapboxgl.accessToken = mapboxToken;
+
+// Prevent auto-scroll to map when popup opens
 const map = new mapboxgl.Map({
   container: "map", // container ID
   center: window.listingCoords, // starting position [lng, lat]. Note that lat must be set between -90 and 90
@@ -19,5 +21,12 @@ const popup = new mapboxgl.Popup({
   .setHTML( 
     "<p>Exact Location provided after booking.</p>")
   .addTo(map);
+
+// Prevent auto-scroll to the map by resetting scroll position after DOM updates
+requestAnimationFrame(function() {
+  requestAnimationFrame(function() {
+    window.scrollTo(0, 0);
+  });
+});
 
  
